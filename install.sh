653516a91847e7f18035fae2ea3ff67eddbee945
reinier <reinierdevalk@gmail.com>
2024-05-17 19:11:03 +0100

    CRLF issue

--- install.sh
+++ install.sh
@@ -1,35 +1,14 @@
 #!/bin/bash
 
-# Script that installs tabmapper on your system. It must
-# called from the same folder that folds config.cfg, cp.sh, 
-# and cp.txt. 
-
-# NB: All line endings in a script must be unix style, so that 
-# calling the script in bash does not lead to the following error:
-# 
-# ./my_script.sh: line <n>: $'\r': command not found
-#
-# There are two solutions to this. 
-# 1. (Temporary) Call the following command before calling the script:
-# 
-# $ tr -d '\r' <my_script.sh >my_script.new && mv my_script.new my_script.sh
-#
-# 2. (Permanent) Open the file in Sublime 4, go to Preferences > Settings, 
-# and add the following in the right field:
-#
-# {
-#    "default_line_ending": "unix"
-# }
-
+# Script that installs tabmapper. It must be called from the
+# same folder that folds config.cfg, cp.sh, and cp.txt.
 
 remove_carriage_returns() {
     local file="$1"
-    # Remove any carriage returns
     if grep -q $'\r' "$file"; then
         tr -d '\r' <"$file" >file.new && mv file.new "$file"
     fi
 }
-
 
 handle_file() {
     local file="$1"
@@ -37,6 +16,19 @@ handle_file() {
     # If the file exists, handle it
     if [ -f "$file" ]; then
         # Remove any carriage returns
+        # All line endings in a script called by bash must be Unix style ('\n') 
+        # and not Windows style ('\r\n'), which cause the following error:
+        # 
+        #   ./<my_script>.sh: line <n>: $'\r': command not found
+        #    
+        # Any carriage returns ('\r') must therefore be removed.
+        #
+        # NB: Carriage returns can be avoided by creating the file in Sublime 4,
+        # after adding the following in the right field at Preferences > Settings:
+        #
+        #   {
+        #    "default_line_ending": "unix"
+        #   }
         remove_carriage_returns "$file"
 
         # Make executable (if applicable)
@@ -49,7 +41,6 @@ handle_file() {
         exit 1
     fi
 }
-
 
 echo "Installing ..."
 
@@ -66,16 +57,35 @@ echo "... reading configuration file ... "
 source "$config_file"
 # Escape forward slashes in CODE_PATH
 code_path_esc=$(echo "$CODE_PATH" | sed 's/\//\\\//g')
-#path_path_esc=$(echo "$PATH_PATH" | sed 's/\//\\\//g')
 
 # 3. Set CODE_PATH (replace placeholder with escaped CODE_PATH)
-echo "... setting path ... "
-placeholder="code_path_placeholder"
+echo "... setting path to code ... "
+placeholder="cp_placeholder"
 sed -i "s/$placeholder/$code_path_esc/g" "$cp_file"
 sed -i "s/$placeholder/$code_path_esc/g" "$tm_file"        
 
-# 4. Copy tabmapper to global environment
+# 4. Create the data/ folder
+echo "... creating data/ folder ... "
+tabmapper_path="$CODE_PATH""tabmapper/"
+data_in="$tabmapper_path""data/in/"
+data_in_tab="$tabmapper_path""data/in/tab/"
+data_in_MIDI="$tabmapper_path""data/in/MIDI/"
+data_out="$tabmapper_path""data/out/"
+if [ ! -d "$data_in" ]; then
+    mkdir -p "$data_in"
+fi
+if [ ! -d "$data_in_tab" ]; then
+    mkdir -p "$data_in_tab"
+fi
+if [ ! -d "$data_in_MIDI" ]; then
+    mkdir -p "$data_in_MIDI"
+fi
+if [ ! -d "$data_out" ]; then
+    mkdir -p "$data_out"
+fi
+
+# 5. Copy tabmapper to global environment
 echo "... copying tabmapper to "$PATH_PATH" ..."
 cp "$tm_file" "$PATH_PATH"
 
-echo "... done!"
+echo "done!"
